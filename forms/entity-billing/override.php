a0741c73116a7139133bd7e93bc1dc68fa2ef850
VVolkov833 <vadim.volkov.dev@gmail.com>
2023-06-08 21:18:54 +0200

    cleared not used forms

--- forms/entity-billing/override.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Print something else instead of the form
-*/
-
-// this form is only for the admin part
-unset( $json->fields );
-$override = '';
-
--- forms/entity-gallery/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Process the form data
-*/
-
-// this form is only for the admin part
-$warning = __( 'This form is only for the the admin area', 'fcpfo' );
-return;
